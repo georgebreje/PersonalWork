0e49cc5811c81d5268a62d165aac5bcbb4cbce18
George Breje <george.breje2@gmail.com>
2020-11-12 21:02:16 +0200

    still working on idea of program

---
+++ Agriculture/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agriculture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose action.");
+            string[] domain = { "Check", "Maintanance" , "", "Oils", "Meat", "Milk", "Eggs"};
+            foreach(var aliment in domain)
+                Console.WriteLine($"{aliment}");
+
+        }
+    }
+}
