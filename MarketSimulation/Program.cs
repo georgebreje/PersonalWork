2d2bdde12c7df7e71a87509cb7d1c22ad113ec8e
George Breje <george.breje2@gmail.com>
2020-11-26 19:14:52 +0200

    OOP

---
+++ MarketSimulation/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketSimulation
+{
+
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Market.Advertisment();
+
+        }
+    }
+    public class Dialogue
+    {
+
+        public static void EmployeeMessage()
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+    public class Market
+    {
+        public static void Advertisment()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Support small businesses");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Press ENTER to start shopping");
+            Console.ResetColor();
+        }
+    }
+    public class Employee
+    {
+
+    }
+    public class Customer
+    {
+        Customer Decision = new Customer();
+        public Customer()
+        {
+            Decision = Choice.EntryChoiceAction();
+        }
+    }
+    public class Choice
+    {
+        Choice EntryChoice = new Choice();
+        public Choice()
+        {
+            EntryChoice = EntryChoiceAction();
+        }
+        public static void EntryChoiceAction()
+        {
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+                Dialogue.EmployeeMessage();
+        }
+
+    }
+    public class Preferences
+    { 
+
+    }
+}
--- MarketSimulation/Properties/AssemblyInfo.cs
+++ MarketSimulation/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Agriculture")]
+[assembly: AssemblyTitle("MarketSimulation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Agriculture")]
+[assembly: AssemblyProduct("MarketSimulation")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9973c2d6-40ee-4fd2-8160-ea6ffa1a00b7")]
+[assembly: Guid("4273098c-1152-4a7b-a003-08600deac381")]
 
 // Version information for an assembly consists of the following four values:
 //
